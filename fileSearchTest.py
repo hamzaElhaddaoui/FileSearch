8868114a5d032746b77147db8444b009400c7e06
hamzaElhaddaoui <hamza.el.haddaoui@gmail.com>
2020-02-29 16:34:51 +0100

    openDossier

--- fileSearchTest.py
+++ fileSearchTest.py
@@ -68,9 +68,16 @@ def createSearchableData(root, progressbar):
         progressbar["value"] = 100
         print("commit done!")
 
-def searchDataByName(name, treeFrame):
+def searchDataByName(name, treeFrame, maxAff, itemTrouve, tempsExecution, trierPar):
     for i in treeFrame.get_children():
         treeFrame.delete(i)
+
+    try:
+        maxAffInt = int(maxAff)
+        maxAffInt = maxAffInt if maxAffInt > 0 else None
+    except ValueError:
+        maxAffInt = None
+
     ix = open_dir("indexdir")
 
     # query_str is query string
@@ -81,18 +88,36 @@ def searchDataByName(name, treeFrame):
 
     with ix.searcher(weighting=scoring.Frequency) as searcher:
         query = QueryParser("title", ix.schema).parse(query_str)
-        results = searcher.search(query, terms=True, limit=None)
-        if len(results) > 0:
+        if (trierPar=="Type"):
+            results = searcher.search(query, terms=True, limit=maxAffInt, sortedby="extension")
+            print("sortedby extension")
+        else:
+            results = searcher.search(query, terms=True, limit=maxAffInt)
+            print("sortedBy score")
+
+        if(maxAffInt!=None):
+            max = maxAffInt if maxAffInt < len(results) else len(results)
+        else:
             max = len(results)
+
+        if len(results) > 0:
+            itemTrouve['text'] = "Item trouvé : "+str(len(results))
+            tempsExecution['text'] = "Temps d'execution : " + str(results.runtime) + " s"
             for i in range(max):
                 treeFrame.insert("", "end", text="", values=(results[i]['title'], results[i]['path'], results[i]['extension'], results[i]['tags']))
                 print("File Name: " + results[i]['title'], "Path: "+results[i]['path'], "Extension: " + results[0]['extension'])
         else:
             print("Aucun resultat trouvé !")
 
-def searchDataByType(type, treeFrame):
+def searchDataByType(type, treeFrame, maxAff, itemTrouve, tempsExecution):
     for i in treeFrame.get_children():
         treeFrame.delete(i)
+
+    try:
+        maxAffInt = int(maxAff)
+        maxAffInt = maxAffInt if maxAffInt > 0 else None
+    except ValueError:
+        maxAffInt = None
 
     ix = open_dir("indexdir")
 
@@ -100,19 +125,33 @@ def searchDataByType(type, treeFrame):
     print('Le type a chercher ', type)
     with ix.searcher(weighting=scoring.Frequency) as searcher:
        query= QueryParser("extension", ix.schema).parse(query_str)
-       results = searcher.search(query, terms=True, limit=None)
+       results = searcher.search(query, terms=True, limit=maxAffInt)
+       if(maxAffInt!=None):
+           max = maxAffInt if maxAffInt < len(results) else len(results)
+       else:
+           max = len(results)
        if len(results) > 0:
-           for i in range(len(results)):
-               treeFrame.insert("", "end", text="", values=(results[i]['title'], results[i]['path'],results[i]['extension'],results[i]['tags']))
+           itemTrouve['text'] = "Item trouvé : " + str(len(results))
+           tempsExecution['text'] = "Temps d'execution : " + str(results.runtime) + " s"
+           for i in range(max):
+               treeFrame.insert("", "end", text="", values=(results[i]['title'], results[i]['path'], results[i]['extension'],results[i]['tags']))
+
                print("File Name: " + results[i]['title'], "Path: " + results[i]['path'], "Contenu: "+results[i]['tags'],
                      "Extension: " + results[i]['extension'])
            print(results)
        else:
             print("Aucun resultat trouvé !")
 
-def searchByContent(content, treeFrame):
+
+def searchByContent(content, treeFrame, maxAff, itemTrouve, tempsExecution):
     for i in treeFrame.get_children():
         treeFrame.delete(i)
+
+    try:
+        maxAffInt = int(maxAff)
+        maxAffInt = maxAffInt if maxAffInt> 0 else None
+    except ValueError:
+        maxAffInt = None
 
     ix = open_dir("indexdir")
 
@@ -120,10 +159,15 @@ def searchByContent(content, treeFrame):
     print('Le contenue a recherche est ', content)
     with ix.searcher(weighting=scoring.Frequency) as searcher:
         query = QueryParser("tags", ix.schema).parse(query_str)
-        results = searcher.search(query, terms=True, limit=None)
-        print(results)
+        results = searcher.search(query, terms=True, limit=maxAffInt)
+        if(maxAffInt != None):
+            max = maxAffInt if maxAffInt < len(results) else len(results)
+        else:
+            max=len(results)
+        itemTrouve['text'] = "Item trouvé : " + str(len(results))
+        tempsExecution['text'] = "Temps d'execution : " + str(results.runtime) + " s"
         if len(results) > 0:
-            for i in range(len(results)):
+            for i in range(max):
                 treeFrame.insert("", "end", text="", values=(
                 results[i]['title'], results[i]['path'], results[i]['extension'], results[i]['tags']))
                 print("File Name: " + results[i]['title'], "Path: " + results[i]['path'], "Contenu: " + results[i]['tags'],
@@ -150,14 +194,15 @@ def searchDataByNameAndType(name, type):
             print("Aucun resultat trouvé !")
 
 
-def chercher(input,type, treeFrame):
+def chercher(input,type, treeFrame, maxAff, itemTrouve, tempsExecution, buttonExp, trierPar):
+    buttonExp['state'] = "disabled"
     if type == "Nom fichier":
         print(input.get())
-        searchDataByName(input.get(), treeFrame)
+        searchDataByName(input.get(), treeFrame, maxAff, itemTrouve, tempsExecution, trierPar)
     elif type == "Type fichier":
-        searchDataByType(input.get(), treeFrame)
+        searchDataByType(input.get(), treeFrame, maxAff, itemTrouve, tempsExecution)
     elif type == "Contenu":
-        searchByContent(input.get(), treeFrame)
+        searchByContent(input.get(), treeFrame, maxAff, itemTrouve, tempsExecution)
 
 class GenererBD(Thread):
     def __init__(self, root, bottonUpdate, bottonChercher, bareProgress):
@@ -193,26 +238,35 @@ def browse_button(buttonUpdate, labelPath):
     print(root)
     buttonUpdate['state'] = "normal"
 
+
+
 def afficher_graphique():
+
     fenetre = Tk()
     fenetre.title('Search file')
-    fenetre.minsize(900, 570)
+    fenetre.minsize(925, 650)
     fenetre.resizable(0, 0)
 
     recherche = Frame(fenetre, width=500, height=200, borderwidth=2)
     recherche.pack()
 
-    champ_label = Label(recherche, text="Entre le terme a chercher:", anchor="w", width=25, height=3)
+    rechercheLabel=Label(recherche, text="La recherche", anchor="w", font=("Courier", 14), height=2)
+    rechercheLabel.grid(row=0, column=0)
+    champ_label = Label(recherche, text="Entrez le terme à chercher :", anchor="w", width=20)
     champ_label.grid(row=1, column=0)
     # champ_label.pack(side="top", fill=Y)
     var_text = StringVar()
     ligne_text = Entry(recherche, textvariable=var_text, width=60)
     print(var_text)
     ligne_text.grid(row=1, column=1)
-    margeLabel=Label(recherche, width=10)
+    margeLabel=Label(recherche, text="TopN :", anchor="w", width=8)
     margeLabel.grid(row=1, column=2)
-    boutton_chercher = Button(recherche, text="Chercher", command=lambda: chercher(var_text, tkvar.get(), fileTable))
-    boutton_chercher.grid(row=1, column=4)
+    var_topN=StringVar()
+    topN_entry=Entry(recherche, textvariable=var_topN, width=15)
+    topN_entry.grid(row=1, column=3)
+    boutton_chercher = Button(recherche, text="Chercher", command=lambda: chercher(var_text, tkvar.get(), fileTable, var_topN.get(),
+                                                                                   itemTrouveValeur, tempsExecutionValeur, label1, typetrier.get()))
+    boutton_chercher.grid(row=1, column=5)
 
     # Create a Tkinter variable
     tkvar = StringVar()
@@ -220,15 +274,37 @@ def afficher_graphique():
     choices = {'Nom fichier', 'Type fichier', 'Contenu'}
     tkvar.set('Nom fichier')
     popupMenu = OptionMenu(recherche, tkvar, *choices)  # set the default option
-    popupMenu.grid(row=1, column=3)
+    popupMenu.grid(row=1, column=4)
+
+    itemTrouveValeur = Label(recherche, text="", anchor="w")
+    itemTrouveValeur.grid(row=2, column=0)
+
+    tempsExecutionValeur = Label(recherche, text="", anchor="w")
+    tempsExecutionValeur.grid(row=2, column=1)
+
+    trierLabel=Label(recherche, text="Triez par :", anchor="w")
+    trierLabel.grid(row=2, column=2)
+
+    # Create a Tkinter variable
+    typetrier = StringVar()
+    # Dictionary with options
+    choicesType = {'Fréquence', 'Date', 'Type', 'Nom'}
+    typetrier.set('Fréquence')
+    popupMenu = OptionMenu(recherche, typetrier, *choicesType)  # set the default option
+    popupMenu.grid(row=2, column=3)
+
 
     indexation = Frame(fenetre, width=500, height=200, borderwidth=2)
     indexation.pack()
 
-    labelIndexation=Label(indexation, text="L'indexation :", anchor="w", width=20, height=3)
-    labelIndexation.grid(row=1, column=0)
 
-    labelPath=Label(indexation, text="", anchor="w", width=35, height=3)
+
+    labelIndexation=Label(indexation, text="L'indexation", anchor="w", font=("Courier",14), width=18, height=2)
+    labelIndexation.grid(row=0, column=0)
+
+    labelChemin=Label(indexation, text="Chemin de dossier Indexé: ", anchor="w", width=28, height=2)
+    labelChemin.grid(row=1, column=0)
+    labelPath=Label(indexation, text="", anchor="w", width=35)
     labelPath.grid(row=1, column=1)
     labelPath['text'] = root
 
@@ -247,10 +323,21 @@ def afficher_graphique():
     treeviewFrame = Frame(fenetre)
     treeviewFrame.pack()
 
-    fileTable = ttk.Treeview(treeviewFrame, height=20)
+    fileTable = ttk.Treeview(treeviewFrame, height=17)
     fileTable['columns'] = ("Nom fichier", "Path", "Extension", "Contenu")
-    fileTable.grid(row=2, column=1, columnspan=5)
+    fileTable.grid(row=2, column=1, columnspan=5, pady=20)
     fileTable.heading("#0", text="", anchor="w")
+
+    def selectItem(event):
+        global pathfile
+        curItem = fileTable.focus()
+        label1['state'] = "normal"
+        obj = fileTable.item(curItem)
+        print(obj.get("values")[1])
+        pathfile = obj.get("values")[1].replace("\\"+obj.get("values")[0], "")
+        print(pathfile)
+
+    fileTable.bind('<ButtonRelease-1>', selectItem)
     fileTable.column("#0", anchor="center", width=5, stretch=NO)
     fileTable.heading("Nom fichier", text="Nom fichier", anchor="w")
     fileTable.column("Nom fichier", anchor="w", width=240)
@@ -264,12 +351,26 @@ def afficher_graphique():
     fileTable.configure(yscroll=EmployViewScrollbar.set)
     EmployViewScrollbar.grid(row=2, column=6, sticky="ns")
 
+    def open():
+        tmp = "F:/Ensias/ENSIAS - 2A/9raya S3\Administration Unix\TP\tpunix_romadi1.txt"
+
+        gotoFenetre='start "" "'+pathfile+'"'
+        print(gotoFenetre)
+        os.system(gotoFenetre)
+
+
+    label1 = Button(fenetre, text="Ouvrir le dossier contenant le fichier", anchor="w", fg="red", font=("Courier", 9, "bold"), command=open)
+    label1.pack()
+    label1['state'] = "disabled"
+
+
     fenetre.mainloop()
 
 
 fichierP = open("DirectoryIndexed.txt", "r")
 root = fichierP.read()
 fichierP.close()
+
 # createSearchableData(root)
 # searchData()
 if len(sys.argv) < 2 or len(sys.argv) > 5:
@@ -283,28 +384,25 @@ if len(sys.argv) < 2 or len(sys.argv) > 5:
     print(len(sys.argv))
     print(sys.argv[1])
     print(sys.argv[3])
-#elif sys.argv[1] == '-c':
+"""
+elif sys.argv[1] == '-c':
 
- #   createSearchableData(root)
+    createSearchableData(root, "create")
 
-#elif len(sys.argv) == 3 and sys.argv[1] == '-name':
-    #searchDataByName(sys.argv[2])
+elif len(sys.argv) == 3 and sys.argv[1] == '-name':
+    searchDataByName(sys.argv[2])
 
-#elif len(sys.argv) == 3 and sys.argv[1] == '-type':
-#    searchDataByType(sys.argv[2])
-#elif len(sys.argv) == 3 and sys.argv[1] == '-content':
-#    searchByContent(sys.argv[2])
-# elif len(sys.argv) == 3 and sys.argv[1] == "-content":
-#     searchByContent(sys.argv[2])
-
-#elif len(sys.argv) == 5 and sys.argv[1] == '-type' and sys.argv[3] == '-name':
-#    searchDataByNameAndType(sys.argv[4], sys.argv[2])
-#elif len(sys.argv) == 5 and sys.argv[1] == '-name' and sys.argv[3] == '-type':
-#    searchDataByNameAndType(sys.argv[2], sys.argv[4])
-
-#fenetre.rowconfigure(0, weight=1)
-#fenetre.columnconfigure(0, weight=1)
-#typeDeRecherche = "nom"
+elif len(sys.argv) == 3 and sys.argv[1] == '-type':
+    searchDataByType(sys.argv[2])
+elif len(sys.argv) == 3 and sys.argv[1] == '-content':
+    searchByContent(sys.argv[2])
+elif len(sys.argv) == 3 and sys.argv[1] == "-content":
+    searchByContent(sys.argv[2])
+elif len(sys.argv) == 5 and sys.argv[1] == '-type' and sys.argv[3] == '-name':
+    searchDataByNameAndType(sys.argv[4], sys.argv[2])
+elif len(sys.argv) == 5 and sys.argv[1] == '-name' and sys.argv[3] == '-type':
+    searchDataByNameAndType(sys.argv[2], sys.argv[4])
+"""
 afficher_graphique()
 
 
